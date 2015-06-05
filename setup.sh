1368f4046042792538c912f08e7f4ca67855ef51
Jorrit Poelen <jhpoelen@gmail.com>
2015-06-05 17:40:01 -0400

    downgrade scala

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 wget http://d3kbcqa49mib13.cloudfront.net/spark-1.3.1-bin-hadoop2.6.tgz
-wget www.scala-lang.org/files/archive/scala-2.11.1.deb
+wget www.scala-lang.org/files/archive/scala-2.10.4.deb
 wget http://dl.bintray.com/sbt/debian/sbt-0.13.8.deb
 git clone https://github.com/idigbio-api-hackathon/idigbio-spark.git
 wget https://s.idigbio.org/idigbio-static-downloads/idigbio.zip
