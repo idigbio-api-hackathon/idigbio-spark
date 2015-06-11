110e74f2751cf601e03e4e20219c4ef6f22d68cd
Jorrit Poelen <jhpoelen@gmail.com>
2015-06-11 16:56:08 -0700

    cassandra index tweak

--- src/main/scala/CassandraUtil.scala
+++ src/main/scala/CassandraUtil.scala
@@ -3,7 +3,7 @@ import com.datastax.spark.connector.SomeColumns
 object CassandraUtil {
 
   def checklistTableCreate: String = {
-      s"CREATE TABLE IF NOT EXISTS idigbio.checklist (taxonselector TEXT, wktstring TEXT, taxon TEXT, recordcount int, PRIMARY KEY(taxonselector, wktstring, recordcount, taxon))"
+      s"CREATE TABLE IF NOT EXISTS idigbio.checklist (taxonselector TEXT, wktstring TEXT, taxon TEXT, recordcount int, PRIMARY KEY((taxonselector, wktstring), recordcount, taxon))"
     }
 
     def checklistKeySpaceCreate: String = {
