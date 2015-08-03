d16e7edfedeab52b48503845f922867c5ac710a3
Jorrit Poelen <jhpoelen@gmail.com>
2015-08-03 13:46:43 -0700

    refactor

--- src/main/scala/TaxonFilter.scala
+++ src/main/scala/TaxonFilter.scala
@@ -1,14 +1,11 @@
 
 
 object TaxonFilter {
-
   def taxonFields = List("dwc:kingdom", "dwc:phylum", "dwc:class", "dwc:order", "dwc:family", "dwc:genus", "dwc:scientificName")
-
 
   def hasTaxa(taxa: List[String], record: Map[String, String]): Boolean = {
     taxonFields
-      .flatMap(record get)
-      .filter(taxa.contains(_)).nonEmpty
+      .flatMap(record get).exists(taxa.contains(_))
   }
 
 }
