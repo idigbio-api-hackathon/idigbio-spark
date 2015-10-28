208af236959ccc9f78c6f8ad3403494045efa879
Jorrit Poelen <jhpoelen@gmail.com>
2015-10-27 18:48:41 -0700

    add test for wrapped envelope

--- src/test/scala/SpatialFilter$Test.scala
+++ src/test/scala/SpatialFilter$Test.scala
@@ -3,9 +3,14 @@ import org.scalatest._
 
 class SpatialFilter$Test extends FlatSpec with Matchers {
 
-  "a row with coordinates" should "return true" in {
+  "a row with coordinates in envelope" should "return true" in {
     val row = Map("dwc:decimalLatitude" -> "32.5", "dwc:decimalLongitude" -> "12.3")
     SpatialFilter.locatedIn("ENVELOPE(10,13,40,30)", row) shouldBe true
+  }
+
+  "a row with coordinates in wrapped envelope" should "return true" in {
+    val row = Map("dwc:decimalLatitude" -> "32.5", "dwc:decimalLongitude" -> "120")
+    SpatialFilter.locatedIn("ENVELOPE(106,76,81,7)", row) shouldBe true
   }
 
   "a row with malformed coordinates" should "return false" in {
